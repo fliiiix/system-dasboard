cd56baeba38b699f4591b5dc9f13ea36639631f1
fliiiix <de-ch@hotmail.de>
2016-08-27 21:11:24 +0200

    include RDash for the web ui part

--- server.py
+++ server.py
@@ -1,11 +1,10 @@
-from bottle import route, get, post, run, template, request
+from bottle import route, get, post, run, template, request, static_file
 import sqlite3
 
 con = sqlite3.connect('dasboard.db')
 
 @post('/')
-def index():
-    
+def create_dns_status():
     j = request.json
     cursor = con.cursor()
     cursor.execute("INSERT INTO dns_status (host_id, hosts_md5, resolv_md5, config_md5, local_ip, remote_ip) VALUES (?, ?, ?, ?, ?, ?)", 
@@ -29,5 +28,10 @@ def createdb():
     con.commit()
     return 'yo'
 
-run(host='192.168.17.174', port=8888)
+
+@route('/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root='./web/dist')
+
+run(host='0.0.0.0', port=8888)
 
